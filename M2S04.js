b495b61402446eeea46bccab20f993d1cee3e2fc
David Dutra <dadutra@hotmail.com>
2024-06-03 15:04:03 -0300

    Card03 - filter()

---
+++ M2S04.js
@@ -0,0 +1,19 @@
+//Card01 - reduce()
+let numeros = [8, 7, 6, 10.0, 20, 21, 33, 99, 97, 101, 6.75, 100];
+
+let total = numeros.reduce(function(total, numero){
+    return total + numero;
+}, 0);
+//console.log(total)
+
+
+
+//Card02 - filter()
+let pares = numeros.filter((element) => {
+    if(element % 2 == 0) return element;
+})
+//console.log(pares)
+
+
+
+//Card03
