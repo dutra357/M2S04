0d6167981d349c05ec4ed5de9cc14532801e3cfb
David Dutra <dadutra@hotmail.com>
2024-06-03 14:55:40 -0300

    Card01 - utilizando 'reduce'.

---
+++ M2S03.js
@@ -0,0 +1,11 @@
+//Card01 - reduce
+let numeros = [8.0, 7.5, 6, 10.0, 20, 99,99, 101, 6.75];
+
+let total = numeros.reduce(function(total, numero){
+    return total + numero;
+}, 0);
+//console.log(total)
+
+
+
+//Card02
